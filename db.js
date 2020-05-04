9beda2851ae032e11883c1f37fc4b4d6889d4afe
joooohee <joooohee@kakao.com>
2020-05-04 15:12:43 +0900

    Create and associate a view template with test db

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 325422,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Joohee',
+      email: 'joohee@kakao.com',
+    },
+  },
+  {
+    id: 555422,
+    title: 'Video nice',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Joohee',
+      email: 'joohee@kakao.com',
+    },
+  },
+  {
+    id: 523422,
+    title: 'Video perfect',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 121212,
+      name: 'Joohee',
+      email: 'joohee@kakao.com',
+    },
+  },
+];
