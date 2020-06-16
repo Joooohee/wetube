7fb96c65c7b5157f337cef01eeb0e839536bf49b
joooohee <joooohee@kakao.com>
2020-06-16 17:46:59 +0900

    [ADD] Create Login and Join

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 import routes from "../routes";
 import User from "../models/User";
+import passport from "passport";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -13,8 +14,17 @@ export const postJoin = async (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
-    const user = await User.create();
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
+    }
   }
 };
 
@@ -22,9 +32,10 @@ export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Login" });
 };
 
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
 
 export const logout = (req, res) => {
   // res.render('logout', { pageTitle: 'Logout' });
