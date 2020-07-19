408ea0c12ea5e15348dd1997b8decb81197b67bb
joooohee <joooohee@kakao.com>
2020-07-19 22:09:17 +0900

    [ADD] Recording Video

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,19 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPriview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+let vidieoRecorder;
+
+const handleVideoDate = (event) => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,14 +23,32 @@ const startRecording = async () => {
     videoPriview.srcObject = stream;
     videoPriview.muted = true;
     videoPriview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "Can't record";
-    recordBtn.removeEventListener(startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
+const stopRecording = () => {
+  vidieoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = () => {
+  vidieoRecorder = new MediaRecorder(streamObject);
+  vidieoRecorder.start();
+  vidieoRecorder.addEventListener("dataavailable", handleVideoDate);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
