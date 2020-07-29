034327e3daa072373fa7f6451c328d91ce8bfbb8
joooohee <joooohee@kakao.com>
2020-07-29 14:58:43 +0900

    [ADD] Dynamic URL Settings

--- src/passport.js
+++ src/passport.js
@@ -16,7 +16,7 @@ passport.use(
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
       callbackURL: process.env.PRODUCTION
-        ? `https://devcenter.heroku.com${routes.githubCallback}`
+        ? `https://limitless-reef-00215.herokuapp.com/${routes.githubCallback}`
         : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback,
